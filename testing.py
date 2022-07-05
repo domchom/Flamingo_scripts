8c6b6aed8b0398cff9fe43d2e4e600ed22b14dd8
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-07-05 17:13:03 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,11 @@
 from interact.kkpo import Kkpo
 import time
 
-kkpo = Kkpo('/Volumes/bigData/kkpo_test/song')
+kkpo = Kkpo('/Volumes/bigData/kkpo_test/1_region_2i')
 
 start = time.time()
-kkpo.save_regions(save_vol=True, overwrite=True)
+kkpo.save_regions(save_vol=False, overwrite=True)
 end = time.time()
 print(f'Saved regions in {round(end - start, 3)} seconds')
+
+#kkpo.view_volumes('R0000')
