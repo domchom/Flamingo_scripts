9d40925dda18198dc3db6038069be7cd2bfdb49f
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-07-05 21:57:07 -0500

    Update testing.py
    playing with distributed client

--- testing.py
+++ testing.py
@@ -1,11 +1,14 @@
 from interact.kkpo import Kkpo
 import time
+from dask.distributed import Client
 
-kkpo = Kkpo('/Volumes/bigData/kkpo_test/1_region_2i')
+if __name__ == '__main__':
+    client = Client()
+    kkpo = Kkpo('/Volumes/bigData/kkpo_test/1_region_2i')
 
-start = time.time()
-kkpo.save_regions(save_vol=False, overwrite=True)
-end = time.time()
-print(f'Saved regions in {round(end - start, 3)} seconds')
+    start = time.time()
+    kkpo.save_regions(save_vol=True, save_max=False, overwrite=True)
+    end = time.time()
+    print(f'Saved regions in {round(end - start, 3)} seconds')
 
-#kkpo.view_volumes('R0000')
+    #kkpo.view_volumes('R0000')
