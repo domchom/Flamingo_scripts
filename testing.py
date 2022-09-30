c9d47d063942adedbca1a4e090f1000d11c0207b
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-09-30 12:47:53 -0500

    updte

--- testing.py
+++ testing.py
@@ -1,15 +1,25 @@
 from interact.kkpo import Kkpo
 import time
 from dask.distributed import Client
+import os
+from tqdm import tqdm
+
 
 if __name__ == '__main__':
     client = Client()
     print(client.dashboard_link)
-    kkpo = Kkpo('/Volumes/bigData/kkpo_test/1_region_2i')
+    dirs = os.walk('/Volumes/zs2tb/stims/')
+    dirs = [d[0] for d in dirs]
+    dirs.sort()
+    dirs.pop(0)
+    with tqdm(total=len(dirs)) as pbar:
+        for dir in dirs:
+            kkpo = Kkpo(dir)
 
-    start = time.time()
-    kkpo.save_regions(save_vol=True, save_max=False, overwrite=True)
-    end = time.time()
-    print(f'Saved regions in {round(end - start, 3)} seconds')
+            start = time.time()
+            kkpo.save_regions(save_vol=True, save_max=True, overwrite=True)
+            end = time.time()
+            print(f'Saved regions in {round(end - start, 3)} seconds')
+            pbar.update(1)
 
     #kkpo.view_volumes('R0000')
